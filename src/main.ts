dcb0d0c1b64e3fa1086aa70b917f23e647a69231
Eduardo <eduardo.freitas@e-deploy.com.br>
2020-10-26 14:53:58 -0400

    feat(security): added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import * as cors from 'cors';
+import * as helmet from 'helmet';
 
 import * as config from 'config';
 import { AppModule } from './modules/app/app.module';
 
-const { port } = config.get('server');
+const { port, origins } = config.get('server');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.use(helmet());
+  app.use(cors({ origin: process.env.ORIGINS || origins }));
   await app.listen(process.env.PORT || port);
 }
 bootstrap();
